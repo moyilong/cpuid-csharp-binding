5149380c6beed1492445dcc0dc52ac35b7e0bc33
吟风 <moyilong@foxmail.com>
2024-04-18 11:12:19 +0800

    remvoe docker tty need

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ run()
     local docker_name=$1
     local docker_file=$2
     docker build -t build.image.$docker_name -f $docker_file --progress plain .
-    docker run -it --rm \
+    docker run -i --rm \
         -v ./Dragon.CpuInfo/runtimes:/install \
         -v ./build-libcpuinfo.sh:/build-libcpuinfo.sh:ro \
         -v ./binding.c:/binding.c:ro \
