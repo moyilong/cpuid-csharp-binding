7f589ebe7c078bcfaf602c7564cb47851362c2ba
moyilong <moyilong@foxmail.com>
2024-05-04 09:25:25 +0800

    use build-native dir

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ run()
 {
     local docker_name=$1
     local docker_file=$2
-    docker build -t build.image.$docker_name -f $docker_file --progress plain .
+    docker build -t build.image.$docker_name -f build-native/$docker_file --progress plain .
     docker run -i --rm \
         -v cpuid-source:/source \
         -e SOURCE_DIR=/source \
