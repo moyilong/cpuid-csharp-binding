457a2c660d848ba9142ed8827c87a45fe0f15c2b
moyilong <moyilong@foxmail.com>
2024-06-05 12:23:07 +0800

    1

--- Dragon.CpuInfo/BindingBridge.cs
+++ Dragon.CpuInfo/BindingBridge.cs
@@ -24,6 +24,7 @@ namespace Dragon.CpuInfo
             {
                 if (UseLibCall)
                 {
+                    dataCache = ProcCallGetYaml();
                 }
                 else
                 {
